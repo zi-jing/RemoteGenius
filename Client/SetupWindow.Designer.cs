2e2cdc8ec2e67fc30511e0a41c51550a6414fd6f
Mo_Ink <64246616+Mo-Ink@users.noreply.github.com>
2020-10-05 12:38:50 +0800

    Revert "Revert "Revert "Revert "Revert "G  U  I  初  实  验"""""
    This reverts commit 47d2fe63b74b45b9733fcc12b3d4c947314dd89d.

--- Client/SetupWindow.Designer.cs
+++ Client/SetupWindow.Designer.cs
@@ -28,54 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("宋体", 12F);
-            this.button1.Location = new System.Drawing.Point(391, 204);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 31);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "确定";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("思源黑体 Bold", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(119, 65);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(253, 59);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "zijing nmsl!";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // SetupWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(519, 247);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "SetupWindow";
             this.ShowIcon = false;
             this.Text = "希沃白板小助手";
-            this.Load += new System.EventHandler(this.SetupWindow_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label label1;
     }
 }
 
