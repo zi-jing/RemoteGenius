47d2fe63b74b45b9733fcc12b3d4c947314dd89d
Mo_Ink <64246616+Mo-Ink@users.noreply.github.com>
2020-10-05 12:38:48 +0800

    Revert "Revert "Revert "Revert "G  U  I  初  实  验""""
    This reverts commit 685f6347e843e689b969aa2cfd7ac36ec1b37a66.

--- Client/SetupWindow.cs
+++ Client/SetupWindow.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using SharedLibrary;
+using System.Windows.Forms;
 
 namespace Client
 {
@@ -7,6 +8,21 @@ namespace Client
         public SetupWindow()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            Util.Dialog("希沃阴间小助手", "你确定你马呢？傻逼！", MessageBoxButtons.OK,MessageBoxIcon.Warning);
+        }
+
+        private void label1_Click(object sender, System.EventArgs e)
+        {
+            Util.Dialog("希沃阴间小助手", "你点哪呢？傻逼！", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void SetupWindow_Load(object sender, System.EventArgs e)
+        {
+
         }
     }
 }
